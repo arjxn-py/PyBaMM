04117b729dce61339e3a7482b85fdece4fdc4b39
arjxn.py <arjxn.py@gmail.com>
2023-06-04 00:44:53 +0530

    Migrate `coverage` to `nox`

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import sys
 
 
 @nox.session(name="pybamm-requires")
-def run_unit_tests(session):
+def run_pybamm_requires(session):
     if sys.platform != "win32" or sys.platform != "darwin":
         session.install("wget", "cmake")
         session.run("python", "scripts/install_KLU_Sundials.py")
@@ -15,3 +15,15 @@ def run_unit_tests(session):
             "pybind11/",
             external=True,
         )
+
+@nox.session(name="coverage")
+def run_coverage(session):
+    homedir = os.getenv("HOME")
+    session.env["SUNDIALS_INST"] = session.env.get("SUNDIALS_INST", f"{homedir}/.local")
+    session.env["LD_LIBRARY_PATH"] = f"{homedir}/.local/lib:{session.env.get('LD_LIBRARY_PATH')}"
+    session.install("coverage","scikits.odes")
+    if sys.platform != "win32" or sys.platform != "darwin":
+        session.run("pybamm_install_jax")
+    session.run("coverage", "run", "run-tests.py", "--nosub")
+    session.run("coverage", "combine")
+    session.run("coverage", "xml")
